aa22511962cbcefe87fc915b710d3f041179aaca
moonlight <moonlightwork2016@outlook.com>
2016-02-29 10:04:27 +0800

    Commit
    Commit

---
+++ StateMode/StateMode/GumballMachine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMode
+{
+    public enum GMState
+    {
+        SOLD_OUT    = 0,
+        NO_QUARTER  = 1,
+        HAS_QUARTER = 2,
+        SOLD        = 3
+    }
+
+    class GumballMachine
+    {
+        public  GMState _state = GMState.SOLD_OUT;
+        public int _count = 0;
+
+        public GumballMachine(int count)
+        {
+            this._count = count;
+
+            if (count > 0)
+            {
+                _state = GMState.NO_QUARTER;
+            }
+        }
+        
+        // 对糖果机的操作接口 来改变内部状态.
+    }
+}
